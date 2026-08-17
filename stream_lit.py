e7cc9bb9c9748582bb0520996b7c0596cd6c9074
MahrukhRanjha <139041093+MahrukhRanjha@users.noreply.github.com>
2026-08-17 06:24:57 -0500

    Add files via upload

---
+++ stream_lit.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import streamlit as st
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+st.title("Talk to Agent")
+
+user_input = st.text_input("Ask a question:")
+
+if st.button("Submit"):
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents=user_input
+    )
+
+    st.write(response.text)
